cfb08781109578eb382b9fdbe12f8bfabdb154f1
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2024-01-28 13:26:23 +0530

    moved the fetch logic in userAtom

--- frontend/src/sources/atoms/usersAtom.js
+++ frontend/src/sources/atoms/usersAtom.js
@@ -1,6 +1,23 @@
 import {atom} from 'recoil'
+import { baseBackendUrl } from '../../../shared/urls'
 
+
+const fetchAllUsers = async()=>{
+    const response = await fetch(`${baseBackendUrl}/user/all-users`,{
+        method: "GET",
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+    })
+    const data = await response.json()
+    console.log(data)
+    return (data.users)
+}
 export const usersAtom = atom({
     key:"userAtom",
-    default: []
+    default: fetchAllUsers()
 })
+
+
+
